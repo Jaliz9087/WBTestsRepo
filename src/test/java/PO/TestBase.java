5171ab206bef65a7b28e266738e7a7a0a01135c9
whey <jimmy.hole@mail.ru>
2025-02-02 22:53:24 +0300

    2 tests added

---
+++ src/test/java/PO/TestBase.java
@@ -0,0 +1,46 @@
+//package PO;
+//
+////import attach.Screen;
+//import com.codeborne.selenide.Configuration;
+//import com.codeborne.selenide.Selenide;
+//import com.codeborne.selenide.logevents.SelenideLogger;
+//import io.qameta.allure.selenide.AllureSelenide;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.openqa.selenium.remote.DesiredCapabilities;
+//
+//import java.util.Map;
+//
+//public class TestBase {
+//    @BeforeAll
+//    static void beforeAll() {
+//        Configuration.browserSize = "1920x1080";
+//        Configuration.timeout = 10000;
+//        Configuration.browser = System.getProperty("browser");
+//        Configuration.browserSize = System.getProperty("browserSize");
+//        Configuration.browserVersion = System.getProperty("browserVersion", "126");
+//        Configuration.baseUrl = "https://demoqa.com";//поменятьx
+//        Configuration.pageLoadStrategy = "eager";
+//        Configuration.remote = "https://user1:1234@" + System.getProperty("remoteHost") + "/wd/hub";
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+//                "enableVNC", true,
+//                "enableVideo", true
+//
+//        ));
+//        Configuration.browserCapabilities = capabilities;
+//    }
+//    @BeforeEach
+//    void logger(){
+//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+//    }
+////    @AfterEach
+////    void getAttach(){
+////        Screen.screenshotAs("Screen after each");
+////        Screen.pageSource();
+////        Screen.browserConsoleLogs();
+////        Screen.addVideo();
+////        Selenide.closeWebDriver();
+////    }
+//}
