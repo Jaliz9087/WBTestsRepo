eb4f503f2c0311a48e8b2afa88d43fc1ba364b92
whey <jimmy.hole@mail.ru>
2025-04-06 23:44:45 +0300

    set timeout

--- src/test/java/uitests/TestBase.java
+++ src/test/java/uitests/TestBase.java
@@ -15,9 +15,9 @@ import java.util.Map;
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
-        Configuration.timeout = 30000;
+        Configuration.timeout = 60000;
         Configuration.browserSize = System.getProperty("browserSize");
-        Configuration.pageLoadTimeout = 40000;
+        Configuration.pageLoadTimeout = 60000;
 
         Configuration.remote = "https://user1:1234@" + System.getProperty("remoteHost") + "/wd/hub";
         Configuration.browser = System.getProperty("browser");
